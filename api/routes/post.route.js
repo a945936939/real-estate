82a261bb02194632e052274daae99077e454922e
a945936939 <945936939@qq.com>
2024-05-01 16:16:31 +1000

    Initial commit

---
+++ api/routes/post.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+const router = express.Router();
+
+// router.get("/test", (req, res) => {
+//   console.log("it worked!");
+// });
+// router.post("/test", (req, res) => {
+//   console.log("it worked!");
+// });
+// router.put("/test", (req, res) => {
+//   console.log("it worked!");
+// });
+// router.delete("/test", (req, res) => {
+//   console.log("it worked!");
+// });
+
+export default router;
